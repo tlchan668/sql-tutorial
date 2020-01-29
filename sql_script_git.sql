0007f8b9bcc7fedd12fa7ffe7e15be15ae8b795f
Traci Chan <tlc668@gmail.com>
2020-01-29 16:41:15 -0500

    added class instructor script

--- sql_script_git.sql
+++ sql_script_git.sql
@@ -25,130 +25,45 @@ select student.id, concat(firstname, ' ', lastname) as 'Full Name', gpa, sat,
 	from Student 
 	left join Major
 		on major.Id = Student.MajorId;
+--regurlar join view class insturctor table sort by last name of instructor
+select  concat(firstname,' ', lastname) as 'Instructor', YearsExperience, 
+		class.id as 'Course Code', 
+		concat(subject, ' ', section) as 'class'
+	from Instructor 
+	join class 
+	on Instructor.id = class.InstructorId
+	order by instructor.Lastname;
 
+--make an alias for tables
+select  concat(firstname,' ', lastname) as 'Instructor', YearsExperience, 
+		c.id as 'Course Code', 
+		concat(subject, ' ', section) as 'class'
+	from Instructor i
+	join class c
+	on i.id = c.InstructorId
+	order by i.Lastname; 
 
--- git stuff
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos
-$ mkdir sql-tutorial
+---only want tenured on this list
+select  concat(firstname,' ', lastname) as 'Instructor', YearsExperience, 
+		c.id as 'Course Code', i.istenured,
+		concat(subject, ' ', section) as 'class'
+	from Instructor i
+	join class c
+	on i.id = c.InstructorId
+	where i.IsTenured=1
+	order by i.Lastname; 
 
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos
-$ cd sql
-SQL_Day2      sql-tutorial/
+---only want not tenured on this list using not
+select  concat(firstname,' ', lastname) as 'Instructor', YearsExperience, 
+		c.id as 'Course Code', i.istenured,
+		concat(subject, ' ', section) as 'class'
+	from Instructor i
+	join class c
+	on i.id = c.InstructorId
+	where not i.IsTenured=1
+	order by i.Lastname; 
 
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos
-$ cd sql-tutorial/
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial
-$ get init
-bash: get: command not found
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial
-$ git init
-Initialized empty Git repository in C:/repos/sql-tutorial/.git/
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
-$ ls
-my-first-sql-scripts.sql
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
-$ git status
-On branch master
-
-No commits yet
-
-Untracked files:
-  (use "git add <file>..." to include in what will be committed)
-        my-first-sql-scripts.sql
-
-nothing added to commit but untracked files present (use "git add" to track)
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
-$ cat my-first-sql-scripts.sql
-select * --id,concat(firstname,' ',lastname) as 'Full Name',gpa,sat
-from student
-where gpa < 3.0
-order by SAT desc;
-
-select * from student;
-select * from major;
---inner join  doesn't show null for min sat required
-select student.id, concat(firstname, ' ', lastname) as 'Full Name', gpa, sat,
-        Description as 'Major', MinSAT as 'min SAT required'
-        from Student
-        join Major
-                on major.Id = Student.MajorId;
-
---outerjoin shows all even null fields
-select student.id, concat(firstname, ' ', lastname) as 'Full Name', gpa, sat,
-        Description as 'Major', MinSAT as 'min SAT required'
-        from Student
-        left join Major
-                on major.Id = Student.MajorId;
---outerjoin takes values of null and rename
-select student.id, concat(firstname, ' ', lastname) as 'Full Name', gpa, sat,
-        isnull(Description, 'Undecided') as 'Major',
-        isnull(MinSAT, '') as 'min SAT required'
-        from Student
-        left join Major
-                on major.Id = Student.MajorId;
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
-$ git add .
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
-$ git status
-On branch master
-
-No commits yet
-
-Changes to be committed:
-  (use "git rm --cached <file>..." to unstage)
-        new file:   my-first-sql-scripts.sql
-
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
-$ git commit -m "Initial Commit"
-[master (root-commit) 7002df4] Initial Commit
- 1 file changed, 27 insertions(+)
- create mode 100644 my-first-sql-scripts.sql
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
-$ git status
-On branch master
-nothing to commit, working tree clean
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
-$ git remote add origin https://github.com/tlchan668/sql-tutorial.git
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
-$ git remote -v
-origin  https://github.com/tlchan668/sql-tutorial.git (fetch)
-origin  https://github.com/tlchan668/sql-tutorial.git (push)
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
-$ git log
-commit 7002df496f905303291b987cb1761474b8490349 (HEAD -> master)
-Author: Traci Chan <tlc668@gmail.com>
-Date:   Wed Jan 29 14:49:22 2020 -0500
-
-    Initial Commit
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
-$ ^C
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
-$ git status
-On branch master
-nothing to commit, working tree clean
-
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
-$ git push origin master
-Enumerating objects: 3, done.
-Counting objects: 100% (3/3), done.
-Delta compression using up to 4 threads
-Compressing objects: 100% (3/3), done.
-Writing objects: 100% (3/3), 541 bytes | 541.00 KiB/s, done.
-Total 3 (delta 0), reused 0 (delta 0)
-To https://github.com/tlchan668/sql-tutorial.git
- * [new branch]      master -> master
- --look back at github and refresh should see work
-MAX-Student@DESKTOP-BU97D2V MINGW64 /c/repos/sql-tutorial (master)
+--is tenured > 3 & is tenured < 7 would be 4, 5, 6
+--istenures <3 & istenured > 7  can't be
+--istenured >3| is tenured < 7  is all numbers
+--age !=5 | age !=7 all numbers   if wanted all numbers but 5 and 7 would write age != 5 & age != 7S
